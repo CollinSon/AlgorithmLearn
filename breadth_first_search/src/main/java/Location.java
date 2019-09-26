104a07fee325fbf21ba6be68e9363f8e8031e654
许瑞锐 <1549359145@qq.com>
2019-09-26 09:21:03 +0800

    广度优先搜索算法代码

---
+++ breadth_first_search/src/main/java/Location.java
@@ -0,0 +1,16 @@
+package PACKAGE_NAME;
+
+/**
+ * @ProjectName: algorithm$
+ * @Package: PACKAGE_NAME$
+ * @ClassName: Location$
+ * @Description: java类作用描述
+ * @Author: 许瑞锐
+ * @CreateDate: 2019/9/25$ 15:14$
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/9/25$ 15:14$
+ * @UpdateRemark: 更新内容
+ * @Version: 1.0
+ */
+public class Location {
+}
